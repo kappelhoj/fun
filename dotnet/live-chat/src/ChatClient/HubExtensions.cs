2e0a3c4beb7065a8699a731db1681094bf02e82c
Arvid Langsø Kappelhøj <a.kappelhoj@gmail.com>
2022-09-20 22:02:09 +0200

    Add todo for next time.

--- dotnet/live-chat/src/ChatClient/HubExtensions.cs
+++ dotnet/live-chat/src/ChatClient/HubExtensions.cs
@@ -5,7 +5,7 @@ namespace ChatClient
 {
     public static class HubExtensions
     {
-
+        //TODO: Wrap On command if possible
         public static async Task InvokeCommandAsync(this HubConnection hub, ServerCommand serverCommand)
         {
             await hub.InvokeAsync(serverCommand.CommandName, serverCommand);

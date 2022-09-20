7d0cb90c3baea32b14b65a89f7ec55817c1f662f
Arvid Langsø Kappelhøj <a.kappelhoj@gmail.com>
2022-09-20 21:55:49 +0200

    More specific typing for sending events to clients

--- dotnet/live-chat/src/ChatServer/Extensions/ClientHubExtensions.cs
+++ dotnet/live-chat/src/ChatServer/Extensions/ClientHubExtensions.cs
@@ -6,7 +6,7 @@ namespace ChatServer.Extensions
     public static class ClientHubExtensions
     {
 
-        public static async Task SendCommandAsync(this IClientProxy proxy, BaseCommand command)
+        public static async Task SendCommandAsync(this IClientProxy proxy, ClientCommand command)
         {
             await proxy.SendAsync(command.CommandName, command);
         }

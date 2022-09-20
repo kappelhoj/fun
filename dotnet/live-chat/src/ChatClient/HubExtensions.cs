29367876e277709811e9893760ac4183ed64b1bb
Arvid Langsø Kappelhøj <a.kappelhoj@gmail.com>
2022-09-20 22:01:45 +0200

    More typing

---
+++ dotnet/live-chat/src/ChatClient/HubExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Chat.Commands.Core;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ChatClient
+{
+    public static class HubExtensions
+    {
+
+        public static async Task InvokeCommandAsync(this HubConnection hub, ServerCommand serverCommand)
+        {
+            await hub.InvokeAsync(serverCommand.CommandName, serverCommand);
+        }
+    }
+}
--- dotnet/live-chat/src/ChatClient/Program.cs
+++ dotnet/live-chat/src/ChatClient/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Chat.Commands;
 using Chat.Commands.ClientCommands;
+using Chat.Commands.Core;
 using Chat.Commands.ServerCommands;
+using ChatClient;
 using Microsoft.AspNetCore.SignalR.Client;
 
 Console.WriteLine("Hello, World!");
@@ -22,8 +24,8 @@ connection.Closed += async (error) =>
 await connection.StartAsync();
 
 //Connection on receive:
-connection.On<ReceiveMessageCommand>("ReceiveMessage", (command) => Console.WriteLine($"[{command.TimeStamp.ToString()}] {command.Username}: {command.Text}"));
+connection.On<ReceiveMessageCommand>(ClientCommandTypes.ReceiveMessage.ToString(), (command) => Console.WriteLine($"[{command.TimeStamp.ToString()}] {command.Username}: {command.Text}"));
 
-await connection.InvokeAsync(ServerCommandTypes.Sendmessage.ToString(), new SendMessageCommand { Username = "kappelhoj", Text = "Hello o/"});
+await connection.InvokeCommandAsync(new SendMessageCommand { Username = "kappelhoj", Text = "Hello o/"});
 
 await Task.Delay(6000);

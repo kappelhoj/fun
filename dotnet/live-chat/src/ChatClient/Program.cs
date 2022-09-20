97e05d073ccaaee24243119daa3abdd0f9d2c6f8
Arvid Langsø Kappelhøj <a.kappelhoj@gmail.com>
2022-09-20 21:12:55 +0200

    Init client to server to client communication

---
+++ dotnet/live-chat/src/ChatClient/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.AspNetCore.SignalR.Client;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Connecting to chathub");
+
+var url = "http://localhost:5229/chatHub";
+
+var connection = new HubConnectionBuilder()
+    .WithUrl(url)
+    .Build();
+
+
+connection.Closed += async (error) =>
+{
+    Console.WriteLine("Connection Closed");
+};
+
+await connection.StartAsync();
+
+//Connection on receive:
+connection.On<string, string>("ReceiveMessage", (user,message) => Console.WriteLine($"{user}: {message}"));
+
+await connection.InvokeAsync("Sendmessage", "kappelhoj", "Hello server!");
+
---
+++ dotnet/live-chat/src/ChatServer/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatServer
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            Console.WriteLine($"Message received from '{user}': {message}");
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}

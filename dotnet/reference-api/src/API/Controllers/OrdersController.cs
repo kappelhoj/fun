1f7a3fb2e3078b68872c76189030738b7705450f
Arvid Langsø Kappelhøj <a.kappelhoj@gmail.com>
2022-11-28 16:51:31 +0100

    Cleanup controller

--- dotnet/reference-api/src/API/Controllers/OrdersController.cs
+++ dotnet/reference-api/src/API/Controllers/OrdersController.cs
@@ -8,35 +8,8 @@ namespace API.Controllers
     [ApiController]
     public class OrdersController : ControllerBase
     {
-        // GET: api/<OrderController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<OrderController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<OrderController>
         [HttpPost]
         public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<OrderController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<OrderController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
         {
         }
     }
